237abd5395280c0ae25502691015ee9aeb7d3962
Blake Fullerton <fullerton199@gmail.com>
2025-07-27 18:06:56 -0700

    taking out repos

--- examples/basic/frameworks/background_agent/src/aiq_multi_frameworks/__init__.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=unused-import
-# flake8: noqa
-
-# Import any functions which need to be automatically registered here
-from . import brev_agent  # noqa: F401
-from . import github_agent  # noqa: F401
-from . import nemo_agent  # noqa: F401
-from . import register  # noqa: F401
